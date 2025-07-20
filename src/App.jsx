84a26ae2731c49c7861728a5d50f5f04008e48af
Shashi Kiran Perka <shashikira4124@gmail.com>
2025-07-21 03:18:39 +0530

    updating files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
 
+  const [message, setMessage] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { username, password } = formData;
+
+    if (username === "user" && password === "password") {
+      setIsLoggedIn(true);
+    } else {
+      setMessage("Invalid username or password");
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>Login Page</h2>
+
+      {!isLoggedIn && (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              onChange={handleChange}
+              placeholder="username"
+              value={formData.username}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={handleChange}
+              placeholder="password"
+              value={formData.password}
+              required
+            />
+          </div>
+          <button type="submit">Submit</button>
+          {message && <p>{message}</p>}
+        </form>
+      )}
+      {isLoggedIn && <p>Welcome, user!</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
